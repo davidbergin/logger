8bf38ca926a6345cbb7c229da37a15708c04a7bd
David <id@davidbergin.net>
2021-02-18 20:10:38 +0000

    Added a few more handler tests.

--- src/test/java/net/davidbergin/logger/handler/HandlerTest.java
+++ src/test/java/net/davidbergin/logger/handler/HandlerTest.java
@@ -45,5 +45,32 @@ public class HandlerTest {
         Handler one = HandlerFactory.getInstance("hello");
         assertNull(one);
     }
+
+    @Test
+    public void jsonHandlingIsAsExpected() {
+        String json = "{\"activity\" : {\"userName\" : \"Sam\",\"websiteName\" : \"abc.com\",\"activityTypeDescription\" : \"Viewed\",\"signedInTime\" : \"01/13/2020\"}}";
+        Handler jsonHandler = HandlerFactory.getInstance("test.json");
+        String result = jsonHandler.handle(json);
+        String expected = "{\"user\":\"Sam\",\"website\":\"abc.com\",\"activityTypeDescription\":\"Viewed\",\"signedInTime\":\"2020-01-13 00:00:00\"}";
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    public void xmlHandlingIsCorrectForValidXML() {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><activity><userName>Williamson</userName><websiteName>xyz.com</websiteName><activityTypeCode>002</activityTypeCode><loggedInTime>2020-01-13</loggedInTime><number_of_views>10</number_of_views></activity>";
+        Handler xmlHandler = HandlerFactory.getInstance("test.xml");
+        String result = xmlHandler.handle(xml);
+        String expected = "{\"user\":\"Williamson\",\"website\":\"xyz.com\",\"activityTypeDescription\":\"Purchased\",\"signedInTime\":\"2020-01-13 00:00:00\"}";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void xmlHandlingIsCorrectForInValidXML() {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><activity><websiteName>xyz.com</websiteName><activityTypeCode>002</activityTypeCode><loggedInTime>2020-01-13</loggedInTime><number_of_views>10</number_of_views></activity>";
+        Handler xmlHandler = HandlerFactory.getInstance("test.xml");
+        String result = xmlHandler.handle(xml);
+        assertNull(result);
+    }
     
 }
